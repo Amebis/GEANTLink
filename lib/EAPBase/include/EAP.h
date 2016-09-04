cbb35ffaefa65d80e090a886fbed517041a39fbd
Simon Rozman <simon@rozman.si>
2016-09-04 21:48:48 +0200

    Diameter "privacy" flag is called "protected" actually

--- lib/EAPBase/include/EAP.h
+++ lib/EAPBase/include/EAP.h
@@ -633,7 +633,7 @@ namespace eap
     enum diameter_avp_flags_t : unsigned char {
         diameter_avp_flag_vendor    = 0x80, ///< Vendor-ID present
         diameter_avp_flag_mandatory = 0x40, ///< Mandatory
-        diameter_avp_flag_privacy   = 0x20, ///< Privacy Required
+        diameter_avp_flag_protected = 0x20, ///< Protected
     };
 
 
