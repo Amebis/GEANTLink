db683f75c0ada765373fef91ad9d222e2257ef8d
Simon Rozman <simon@rozman.si>
2016-09-07 14:04:00 +0200

    Version set to 1.0-beta1

--- include/Version.h
+++ include/Version.h
@@ -29,7 +29,7 @@
 // Product version as a single DWORD
 // Note: Used for version comparison within C/C++ code.
 //
-#define PRODUCT_VERSION          0x00ff1200
+#define PRODUCT_VERSION          0x00ff1300
 
 //
 // Product version by components
@@ -39,26 +39,26 @@
 //
 #define PRODUCT_VERSION_MAJ      0
 #define PRODUCT_VERSION_MIN      255
-#define PRODUCT_VERSION_REV      18
+#define PRODUCT_VERSION_REV      19
 #define PRODUCT_VERSION_BUILD    0
 
 //
 // Human readable product version and build year for UI
 //
-#define PRODUCT_VERSION_STR      "1.0-beta"
+#define PRODUCT_VERSION_STR      "1.0-beta1"
 #define PRODUCT_BUILD_YEAR_STR   "2016"
 
 //
 // Numerical version presentation for ProductVersion propery in
 // MSI packages (syntax: N.N[.N[.N]])
 //
-#define PRODUCT_VERSION_INST     "0.255.18"
+#define PRODUCT_VERSION_INST     "0.255.19"
 
 //
 // The product code for ProductCode property in MSI packages
 // Replace with new on every version change, regardless how minor it is.
 //
-#define PRODUCT_VERSION_GUID     "{A638F64B-F40A-4397-A212-EEFCD3773F0E}"
+#define PRODUCT_VERSION_GUID     "{E0362E50-F0A0-452D-A382-9592971AC867}"
 
 //
 // Product vendor
