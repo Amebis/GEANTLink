5b452a25fe8242142981f7ade9156c6c1d51e43e
Simon Rozman <simon@rozman.si>
2020-03-03 12:54:15 +0100

    Makefile: Optimize cleaning
    NMAKE invocations are costly (slow, no-parallel support).
    
    Rather than pinpoint cleaning of each combination of $(LANG), $(PLAT)
    and $(CFG) related files, we do the wildcard cleaning of all packaging
    files.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- WLANManager/MSIBuild/clean_all.bat
+++ WLANManager/MSIBuild/clean_all.bat
@@ -1,7 +1,2 @@
 @echo off
-nmake.exe /ls Clean LANG=en_US CFG=Debug   PLAT=Win32
-nmake.exe /ls Clean LANG=en_US CFG=Debug   PLAT=x64
-nmake.exe /ls Clean LANG=en_US CFG=Debug   PLAT=ARM64
-nmake.exe /ls Clean LANG=en_US CFG=Release PLAT=Win32
-nmake.exe /ls Clean LANG=en_US CFG=Release PLAT=x64
-nmake.exe /ls Clean LANG=en_US CFG=Release PLAT=ARM64
+nmake.exe /ls Clean
