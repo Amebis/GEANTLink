b96ebfbce44db583da563b0afae84fc50f373feb
Simon Rozman <simon@rozman.si>
2021-11-22 13:09:17 +0100

    Switch to SPDX license notice
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- lib/TLS/include/Module.h
+++ lib/TLS/include/Module.h
@@ -1,21 +1,7 @@
 /*
-    Copyright 2015-2020 Amebis
-    Copyright 2016 GÉANT
-
-    This file is part of GÉANTLink.
-
-    GÉANTLink is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    GÉANTLink is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with GÉANTLink. If not, see <http://www.gnu.org/licenses/>.
+    SPDX-License-Identifier: GPL-3.0-or-later
+    Copyright Â© 2015-2020 Amebis
+    Copyright Â© 2016 GÃ‰ANT
 */
 
 namespace eap
